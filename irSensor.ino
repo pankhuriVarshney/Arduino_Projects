2e15a4233134d2c4b90d2964fe3467e23e8f1ebe
Patz17 <call.me.bi.your.name17@gmail.com>
2023-08-02 09:24:35 +0530

    Project: Make an IR Sensor Robot that goes forward, right and left turn.
    Setup:
    Include AFmotor package for the dc motors attached to the motor driver
    Add pinMode for digital pins 3 and 2 as INPUT since they will receive input from the IR sensors.
    
    Loop:
    Read digital signals from the IR Sensors
    Add a conditional statement calling forward, rightTurn, leftTurn for respective cases

--- irSensor.ino
+++ irSensor.ino
@@ -1,8 +1,6 @@
 #include<AFMotor.h>
   AF_DCMotor Motor1(1,MOTOR12_1KHZ);
   AF_DCMotor Motor2(4,MOTOR12_1KHZ);
-  //AF_DCMotor Motor3(3,MOTOR34_1KHZ);
-  //AF_DCMotor Motor4(4,MOTOR34_1KHZ);
 
   int RIGHT=3;
   int LEFT=2;
